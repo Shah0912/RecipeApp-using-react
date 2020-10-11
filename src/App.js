04b38ff27321c8926ba96ae4be82044a557e72f4
Shah0912 <adi.shah0912@gmail.com>
2020-10-11 14:21:07 +0530

    Added a sample get req... Added a search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const APP_ID = "ea5bfa4c";
+  const APP_KEY = "171b1ec1c87cf3c7d6d5b177ec0c1c0b";
+  
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
+      <form className = "search-form">
+        <input type = "text" classname = "search-bar"></input>
+        <button type = "submit" classname = "search-button">Search</button>
+      </form>
     </div>
   );
 }
